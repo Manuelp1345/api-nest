a5020e77baa8acf1ef0943b53445e89b2250f77d
manuelp1345 <manuelp1345@gmail.com>
2023-08-17 10:42:38 -0400

    changes cors

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,10 @@ import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const whitelist = [
-    'http://localhost:3000/',
-    'https://api-nest-nu.vercel.app',
-    'https://api-nest-i8iq.onrender.com/',
-  ];
+  const whitelist = ['https://api-nest-i8iq.onrender.com/'];
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: whitelist,
     credentials: true,
   });
   await app.listen(env.PORT || 3000);
