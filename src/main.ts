a79c1ddd4470c315fefe71bd338d86efc255c94e
manuelp1345 <manuelp1345@gmail.com>
2023-08-17 10:59:33 -0400

    graphql playground

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
 import { env } from 'process';
+import graphqlPlayground from 'graphql-playground-middleware-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: 'https://api-nest-i8iq.onrender.com/',
     credentials: true,
     optionsSuccessStatus: 200,
   });
+  app.use('/playground', graphqlPlayground({ endpoint: '/graphql' }));
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(env.PORT || 3000);
 }
 bootstrap();
